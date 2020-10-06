7aae7b9999860ddd0ee155fcb217854f20be5a90
laverboy <matthew.laver@bbc.co.uk>
2020-10-06 15:00:19 +0100

    Added a new vue based site

---
+++ main-site/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Editify"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 1000px;
+}
+</style>
---
+++ main-site/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Enter a web address to see a list of edits
+    </p>
+    <input v-model="siteToLookup" placeholder="www.mysite.com" type="text" v-on:keyup.enter="submit">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: function () {
+    return {
+      siteToLookup: "",
+    }
+  },
+  methods: {
+    submit: function (e) {
+      if (this.siteToLookup === "") {
+        return
+      }
+      console.log(e, this.siteToLookup)
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+input {
+  width: 100%;
+  padding: 10px;
+  font-size: 1.8em;
+}
+
+a {
+  color: #42b983;
+}
+</style>
